43c6075415a7569be5a77095f92a24a136451b8f
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2023-12-26 13:17:27 -0300

    pawn attack

--- app.js
+++ app.js
@@ -103,8 +103,10 @@ function checkIfValid(target){
     switch(piece){
         case 'pawn' :
             const starterRow = [8,9,10,11,12,13,14,15]
-            if(starterRow.includes(startId) && startId + width * 2 === targetId
-            || startId + width === targetId){
+            if(starterRow.includes(startId) && startId + width * 2 === targetId ||
+            startId + width === targetId ||
+            startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild || 
+            startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild ){
                 return true
             }
     }
